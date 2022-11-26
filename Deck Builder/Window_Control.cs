ae4daba76b6a02ff0b4c1389154620929c7ec119
Ken Finlayson <kennef11@gmail.com>
2022-11-26 13:02:00 +1300

    update

---
+++ Deck Builder/Window_Control.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Deck_Builder.DeckCreation;
+
+namespace Deck_Builder
+{
+    static class Window_Control
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //create main menu handler and run it
+            MainMenuHandler menu = new MainMenuHandler();
+            menu.run();
+
+        }
+    }
+}
